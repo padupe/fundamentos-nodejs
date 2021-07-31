e0824c124f1723ae504bb9a712b29af032570164
Paulo Peixoto <pauloeduardo_peixoto@hotmail.com>
2021-07-31 19:43:22 -0300

    feat(index): Extract search by cpf.
    README Update

--- src/index.js
+++ src/index.js
@@ -13,22 +13,35 @@ const customers = []
  */
 
 app.post('/account', (request, response) => {
-    const { cpf, name } = request.body
+    const { cpf, name } = request.body;
 
-    const customerFind = customers.some((customerfind) => customerfind.cpf === cpf)
+    const customerFind = customers.some((customerfind) => customerfind.cpf === cpf);
 
     if (customerFind) {
         return response.status(400).json({ error: 'Customer already exists!'})
-    }
+    };
 
     customers.push({
         cpf,
         name,
         id: uuidv4,
-        statemente: []
-    })
+        statement: []
+    });
 
     return response.status(201).send()
-})
+});
+
+app.get('/statement', (request, response) => {
+    const { cpf } = request.headers;
+
+    const customer = customers.find((customerfind => customerfind.cpf === cpf));
+
+    if(!customer) {
+        return response.status(400).json({ error: 'Customer not found!' });
+    }
+
+    return response.json(customer.statement)
+
+});
 
 app.listen(2601)
