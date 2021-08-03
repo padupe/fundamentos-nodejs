4796900948e554aa92bc45d216b574672eaf5e09
Paulo Peixoto <pauloeduardo_peixoto@hotmail.com>
2021-08-03 14:30:04 -0300

    feat(Middleware): verifyIfExistsAccountCPF

--- src/index.js
+++ src/index.js
@@ -5,12 +5,20 @@ const app = express()
 app.use(express.json())
 const customers = []
 
-/**
- * CPF - String
- * NAME - String
- * ID - uuid
- * STATEMENT (Extrato): [] Array
- */
+// Middleware
+function verifyIfExistsAccountCPF(request, response, next) {
+    const { cpf } = request.headers;
+
+    const customer = customers.find((customerfind => customerfind.cpf === cpf));
+
+    if(!customer) {
+        return response.status(400).json({ error: 'Customer not found!' });
+    }
+
+    request.customer = customer;
+
+    return next();
+}
 
 app.post('/account', (request, response) => {
     const { cpf, name } = request.body;
@@ -31,14 +39,10 @@ app.post('/account', (request, response) => {
     return response.status(201).send()
 });
 
-app.get('/statement', (request, response) => {
-    const { cpf } = request.headers;
+// app.use(verifyIfExistsAccountCPF);
 
-    const customer = customers.find((customerfind => customerfind.cpf === cpf));
-
-    if(!customer) {
-        return response.status(400).json({ error: 'Customer not found!' });
-    }
+app.get('/statement', verifyIfExistsAccountCPF, (request, response) => {
+    const { customer } = request;    
 
     return response.json(customer.statement)
 
